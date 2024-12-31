26676bd45a76928ae2d12f8531931a0d4b371c72
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-12-31 01:47:32 +0100

    Changed nvim color scheme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -624,7 +624,7 @@ require("lazy").setup({
 			--  - cmd (table): Override the default command used to start the server
 			--  - filetypes (table): Override the default list of associated filetypes for the server
 			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-			--  - settings (table): Override the default settings passed when initializing the server.
+			--  - settings (table): Overrp00f/alabaster.nvimide the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				-- clangd = {},
@@ -851,14 +851,15 @@ require("lazy").setup({
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"Verf/deepwhite.nvim",
+		"p00f/alabaster.nvim",
 		lazy = false,
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("deepwhite")
+			vim.o.background = "light"
+			vim.cmd.colorscheme("alabaster")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")

9fd28b24d2ad81107a2520e22cdc4c23ef44cfdd
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-01-03 22:32:53 +0100

    changed lualine colors

--- nvim/.config/nvim/lua/custom/plugins/lualine.lua
+++ nvim/.config/nvim/lua/custom/plugins/lualine.lua
@@ -1,3 +1,21 @@
+local colors = {
+	black = "#000000",
+	white = "#ffffff",
+
+	bg4 = "e0e0e0",
+
+	bg0 = "#f7f7f7",
+	bg1 = "#b7b7b7",
+	bg2 = "#5b5b5b",
+	bg3 = "#2b2b2b",
+
+	blue = "#325CC0",
+	green = "#448C27",
+	red = "#AA3731",
+	magenta = "#7A3E9D",
+	orange = "#FFBC5D",
+}
+
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -5,9 +23,40 @@ return {
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = "alabaster",
+				theme = {
+					normal = {
+						a = { bg = colors.bg3, fg = colors.white, gui = "bold" },
+						b = { bg = colors.bg4, fg = colors.black },
+						c = { bg = colors.bg4, fg = colors.black },
+					},
+					insert = {
+						a = { bg = colors.blue, fg = colors.white, gui = "bold" },
+						b = { bg = colors.bg0, fg = colors.black },
+						c = { bg = colors.bg0, fg = colors.black },
+					},
+					visual = {
+						a = { bg = colors.magenta, fg = colors.white, gui = "bold" },
+						b = { bg = colors.bg0, fg = colors.black },
+						c = { bg = colors.bg0, fg = colors.black },
+					},
+					replace = {
+						a = { bg = colors.red, fg = colors.white, gui = "bold" },
+						b = { bg = colors.bg0, fg = colors.black },
+						c = { bg = colors.bg0, fg = colors.black },
+					},
+					command = {
+						a = { bg = colors.green, fg = colors.white, gui = "bold" },
+						b = { bg = colors.bg0, fg = colors.black },
+						c = { bg = colors.bg0, fg = colors.black },
+					},
+					inactive = {
+						a = { bg = colors.bg1, fg = colors.white, gui = "bold" },
+						b = { bg = colors.bg0, fg = colors.black },
+						c = { bg = colors.bg0, fg = colors.black },
+					},
+				},
 				component_separators = { left = "|", right = "|" },
-				section_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},
@@ -41,7 +90,7 @@ return {
 			tabline = {},
 			winbar = {},
 			inactive_winbar = {},
-			extensions = { "neo-tree" },
+			extensions = {},
 		})
 	end,
 }

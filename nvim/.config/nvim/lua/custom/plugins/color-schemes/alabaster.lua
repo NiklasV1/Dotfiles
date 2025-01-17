21514c1b21fdc5f1f4d756a4c8adc5ca098e317a
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-01-17 23:31:05 +0100

    alabaster

--- nvim/.config/nvim/lua/custom/plugins/color-schemes/alabaster.lua
+++ nvim/.config/nvim/lua/custom/plugins/color-schemes/alabaster.lua
@@ -4,7 +4,7 @@ return {
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"p00f/alabaster.nvim",
+		dir = "~/Programming/Other-Repos/alabaster/alabaster.nvim",
 		lazy = false,
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
@@ -13,9 +13,15 @@ return {
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
 			vim.o.background = "light"
 			vim.cmd.colorscheme("alabaster")
+			vim.highlight.priorities.semantic_tokens = 90
+
+			-- Disable semantic highlights
+			--for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+			--	vim.api.nvim_set_hl(0, group, {})
+			--end
 
 			-- You can configure highlights by doing something like:
-			vim.cmd.hi("Comment gui=none")
+			vim.hl = vim.highlight
 		end,
 	},
 }

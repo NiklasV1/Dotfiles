56d5687de00545fafe0bda5f003934b2da275faa
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-02-23 12:40:47 +0100

    fix(nvim): added default linters and a few comments

--- nvim/.config/nvim/lua/visuals/plugins/init.lua
+++ nvim/.config/nvim/lua/visuals/plugins/init.lua
@@ -1,8 +1,19 @@
 return {
+	-- Color scheme
 	require("visuals.plugins.color-schemes.alabaster"),
+
+	-- Status Line
 	require("visuals.plugins.lualine"),
+
+	-- Gis change signs
 	require("visuals.plugins.gitsigns"),
+
+	-- Indentation lines
 	require("visuals.plugins.indent_line"),
+
+	-- Todo comments and notes highlighting
 	require("visuals.plugins.todo-comments"),
+
+	-- Keymap helper window
 	require("visuals.plugins.which-key"),
 }

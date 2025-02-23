bc1cdaebfe8109fc58b7e3f276c929e52bfb1abf
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-02-23 13:53:35 +0100

    fix(nvim): added correct colors for indent lines

--- nvim/.config/nvim/lua/visuals/plugins/indent_line.lua
+++ nvim/.config/nvim/lua/visuals/plugins/indent_line.lua
@@ -1,9 +1,27 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		opts = {
+			indent = {
+				char = "│",
+				highlight = {
+					"IndentLineDefault",
+				},
+			},
+			scope = {
+				enabled = true,
+				highlight = {
+					"IndentLineScope",
+				},
+			},
+		},
+		config = function(_, opts)
+			vim.api.nvim_set_hl(0, "IndentLineDefault", { fg = "#CECECE" })
+			vim.api.nvim_set_hl(0, "IndentLineScope", { fg = "#325CC0" })
+			require("ibl").setup(opts)
+		end,
+	},
 }

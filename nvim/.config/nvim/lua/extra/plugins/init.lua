5487a93eac002273825017005294679ae4f53fa4
Niklas Vaethbrueckner <n.vaethbrueckner@snapaddy.com>
2025-02-25 09:19:17 +0100

    feat(nvim): added harpoon

--- nvim/.config/nvim/lua/extra/plugins/init.lua
+++ nvim/.config/nvim/lua/extra/plugins/init.lua
@@ -1,4 +1,7 @@
 return {
 	-- Auto close HTML/XML tags
 	require("extra.plugins.ts-autotag"),
+
+	-- Harpoon
+	require("extra.plugins.harpoon"),
 }
--- nvim/.config/nvim/lua/visuals/plugins/which-key.lua
+++ nvim/.config/nvim/lua/visuals/plugins/which-key.lua
@@ -65,6 +65,7 @@ return {
 				{ "<leader>t", group = "[T]oggle" },
 				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
 				{ "<leader>l", group = "[L]azy Git" },
+				{ "<leader>g", group = "[G]o" },
 			},
 		},
 	},

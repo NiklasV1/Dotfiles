41e1a41e8e9c8598026a4098f7ff924f5ea5e84f
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-03-09 19:56:08 +0100

    fix(nvim): added angular form snippets

--- nvim/.config/nvim/lua/core/plugins/cmp.lua
+++ nvim/.config/nvim/lua/core/plugins/cmp.lua
@@ -100,6 +100,16 @@ return {
 							luasnip.jump(-1)
 						end
 					end, { "i", "s" }),
+					vim.keymap.set({ "i", "s" }, "<C-N>", function()
+						if luasnip.choice_active() then
+							luasnip.change_choice(1)
+						end
+					end, { silent = true }),
+					vim.keymap.set({ "i", "s" }, "<C-P>", function()
+						if luasnip.choice_active() then
+							luasnip.change_choice(-1)
+						end
+					end, { silent = true }),
 
 					-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
 					--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
--- nvim/.config/nvim/lua/snippets/angular.lua
+++ nvim/.config/nvim/lua/snippets/angular.lua
@@ -11,14 +11,16 @@ local d = ls.dynamic_node
 local c = ls.choice_node
 
 return {
+	-- Services
 	s(
-		"ng-signal",
-		fmt("protected readonly {name} = signal<{type}>({value});", {
+		"ng-service",
+		fmt("readonly #{name} = inject({service});", {
 			name = i(1, "name"),
-			type = i(2, "type"),
-			value = i(3, "value"),
+			service = i(2, "service"),
 		})
 	),
+
+	-- In/Outputs
 	s(
 		"ng-input",
 		fmt("public readonly {name} = input<{type}>();", {
@@ -26,11 +28,57 @@ return {
 			type = i(2, "type"),
 		})
 	),
+
+	-- Forms
+	s("ng-form-builder", {
+		c(1, {
+			t("readonly #formBuilder = inject(FormBuilder);"),
+			t("readonly #formBuilderNonNullable = inject(NonNullableFormBuilder);"),
+		}),
+	}),
+	s("ng-form", {
+		c(1, {
+			fmt("protected readonly {name} = this.{builder}.{kind}<{type}>({value});", {
+				name = i(1, "name"),
+				builder = c(2, {
+					t("#formBuilder"),
+					t("#formBuilderNonNullable"),
+					i("builder"),
+				}),
+				kind = c(3, {
+					t("control"),
+					t("group"),
+					t("record"),
+					t("array"),
+				}),
+				type = i(4, "type"),
+				value = i(5, "value"),
+			}),
+			fmt("protected readonly {name} = {kind}<{type}>({value});", {
+				name = i(1, "name"),
+				kind = c(2, {
+					t("FormControl"),
+					t("FormGroup"),
+					t("FormRecord"),
+					t("FormArray"),
+				}),
+				type = i(3, "type"),
+				value = i(4, "value"),
+			}),
+			fmt("{name}: [{value}],", {
+				name = i(1, "name"),
+				value = i(2, "value"),
+			}),
+		}),
+	}),
+
+	-- Signals
 	s(
-		"ng-inject",
-		fmt("readonly #{name} = inject({service});", {
+		"ng-signal",
+		fmt("protected readonly {name} = signal<{type}>({value});", {
 			name = i(1, "name"),
-			service = i(2, "service"),
+			type = i(2, "type"),
+			value = i(3, "value"),
 		})
 	),
 	s(

af607b845dbdae2d38b976068af304b924ae7435
Niklas Vaethbrueckner <n.vaethbrueckner@snapaddy.com>
2025-03-14 08:56:37 +0100

    feat(nvim): added Pnpm error parser command

--- nvim/.config/nvim/lua/parsers/pnpm.lua
+++ nvim/.config/nvim/lua/parsers/pnpm.lua
@@ -4,16 +4,25 @@ return {
 		print("...")
 		local quickfixEntries = {}
 		local output = vim.fn.system({ "rushx", "build" })
-		string.gsub(output, "(src/[%w_%-/]*%.[%w_%-%.]*)%((%d*),%d*%): [^:]*: ([^\n]*)", function(path, line, message)
-			table.insert(quickfixEntries, {
-				filename = path,
-				lnum = line,
-				type = "E",
-				text = message,
-			})
-		end)
-		vim.fn.setqflist(quickfixEntries)
+		local _, count = string.gsub(
+			output,
+			"(src/[%w_%-/]*%.[%w_%-%.]*)%((%d*),%d*%): [^:]*: ([^\n]*)",
+			function(path, line, message)
+				table.insert(quickfixEntries, {
+					filename = path,
+					lnum = line,
+					type = "E",
+					text = message,
+				})
+			end
+		)
 		print("Build complete.")
-		vim.api.nvim_command("copen")
+		if count > 0 then
+			vim.fn.setqflist(quickfixEntries)
+			print("Found " .. count .. " errors!")
+			vim.api.nvim_command("copen")
+		else
+			print("No errors found.")
+		end
 	end,
 }

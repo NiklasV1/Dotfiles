770bdf0dd20e3e94ff854efca723613c497b2841
Niklas Vaethbrueckner <n.vaethbrueckner@snapaddy.com>
2025-03-20 18:57:01 +0100

    todo(nvim): eslint parser

--- nvim/.config/nvim/lua/parsers/init.lua
+++ nvim/.config/nvim/lua/parsers/init.lua
@@ -6,4 +6,11 @@ return {
 		[[lua require("parsers.pnpm").parse()]],
 		{ desc = "Parse pnpm output to quickfix list." }
 	),
+
+	-- Eslint_d
+	vim.api.nvim_create_user_command(
+		"Eslint",
+		[[lua require("parsers.eslint").parse()]],
+		{ desc = "Parse eslint output to quickfix list." }
+	),
 }

d949176ac3cde69054da79db2b58164c7e638f6b
Niklas Vaethbrueckner <n.vaethbrueckner@snapaddy.com>
2025-03-24 11:03:50 +0100

    feat(nvim): colorizer refresh autocommand

--- nvim/.config/nvim/lua/visuals/plugins/hexcolors.lua
+++ nvim/.config/nvim/lua/visuals/plugins/hexcolors.lua
@@ -1,9 +1,21 @@
 return {
 	"norcalli/nvim-colorizer.lua",
 	config = function()
-		require("colorizer").setup({
+		local colorizer = require("colorizer")
+
+		colorizer.setup({
 			css = { css = true, mode = "background" },
 			scss = { css = true, mode = "background" },
+		})
+
+		local command_group = vim.api.nvim_create_augroup("colorizer", { clear = true })
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			pattern = { "*.css", "*.scss" },
+			desc = "Refresh colorizer",
+			group = command_group,
+			callback = function()
+				colorizer.reload_all_buffers()
+			end,
 		})
 	end,
 }

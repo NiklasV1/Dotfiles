293b0ea483e4527ee485eb5d554cae08c64e2888
Niklas Vaethbrueckner <n.vaethbrueckner@snapaddy.com>
2025-04-04 16:31:45 +0200

    fix(nvim): removed sql completions

--- nvim/.config/nvim/lua/core/plugins/cmp.lua
+++ nvim/.config/nvim/lua/core/plugins/cmp.lua
@@ -34,6 +34,8 @@ return {
 			--  into multiple repos for maintenance purposes.
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
+			"ray-x/cmp-sql",
 		},
 		config = function()
 			-- See `:help cmp`
@@ -128,6 +130,8 @@ return {
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
 					{ name = "path" },
+					{ name = "buffer" },
+					-- { name = "sql" },
 				},
 			})
 		end,

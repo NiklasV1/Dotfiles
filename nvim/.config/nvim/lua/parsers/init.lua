eab79b9a7252dda6126cb4daf6ddf900e89ed2ea
Niklas Vaethbrueckner <n.vaethbrueckner@snapaddy.com>
2025-04-04 16:34:56 +0200

    fix(nvim): changed rush commands

--- nvim/.config/nvim/lua/parsers/init.lua
+++ nvim/.config/nvim/lua/parsers/init.lua
@@ -1,9 +1,16 @@
 return {
 
-	-- PNPM
+	-- Rushx feature build
 	vim.api.nvim_create_user_command(
-		"Pnpm",
+		"RushxFix",
 		[[lua require("parsers.pnpm").parse()]],
+		{ desc = "Parse pnpm output to quickfix list." }
+	),
+
+	-- Rush build
+	vim.api.nvim_create_user_command(
+		"Rush",
+		[[lua require("parsers.rush").parse()]],
 		{ desc = "Parse pnpm output to quickfix list." }
 	),
 
@@ -12,6 +19,11 @@ return {
 		"Jest",
 		[[lua require("parsers.jest").parse()]],
 		{ desc = "Parse jest output to quickfix list." }
+	),
+	vim.api.nvim_create_user_command(
+		"JestCurrent",
+		[[!npm exec jest -- --testMatch='**/%:t']],
+		{ desc = "Run current jest test" }
 	),
 
 	-- Eslint_d

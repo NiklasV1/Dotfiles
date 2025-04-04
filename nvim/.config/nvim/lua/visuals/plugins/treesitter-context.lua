b828110a2e75cbf7b94f231dce1908051da350d1
Niklas Vaethbrueckner <n.vaethbrueckner@snapaddy.com>
2025-04-04 18:21:18 +0200

    feat(nvim): changed treesitter context colors

--- nvim/.config/nvim/lua/visuals/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/visuals/plugins/treesitter-context.lua
@@ -8,11 +8,11 @@ return {
 
 		vim.api.nvim_set_hl(0, "TreesitterContext", { bg = colors.white })
 		vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { bg = colors.white })
-		vim.api.nvim_set_hl(0, "TreesitterContextBottom", { bg = colors.redLight2 })
+		vim.api.nvim_set_hl(0, "TreesitterContextBottom", { bg = colors.fgLight2 })
 		vim.api.nvim_set_hl(
 			0,
 			"TreesitterContextLineNumberBottom",
-			{ bg = colors.palette.red.bright, fg = colors.white, bold = true }
+			{ bg = colors.palette.fg.default, fg = colors.white, bold = true }
 		)
 		require("treesitter-context").setup({})
 	end,

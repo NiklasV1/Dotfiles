36e03c6c17cc0305b0a1f23e60477efff18cc1e2
Niklas Vaethbrueckner <n.vaethbrueckner@snapaddy.com>
2025-04-29 16:58:42 +0200

    feat(nvim): added grapple hotkeys

--- nvim/.config/nvim/lua/extra/plugins/grapple.lua
+++ nvim/.config/nvim/lua/extra/plugins/grapple.lua
@@ -14,6 +14,11 @@ return {
 		{ "<leader>g3", "<cmd>Grapple select index=3<cr>", desc = "[G]rapple [3]" },
 		{ "<leader>g4", "<cmd>Grapple select index=4<cr>", desc = "[G]rapple [4]" },
 		{ "<leader>g5", "<cmd>Grapple select index=5<cr>", desc = "[G]rapple [5]" },
+		{ "<leader>g6", "<cmd>Grapple select index=6<cr>", desc = "[G]rapple [6]" },
+		{ "<leader>g7", "<cmd>Grapple select index=7<cr>", desc = "[G]rapple [7]" },
+		{ "<leader>g8", "<cmd>Grapple select index=8<cr>", desc = "[G]rapple [8]" },
+		{ "<leader>g9", "<cmd>Grapple select index=9<cr>", desc = "[G]rapple [9]" },
+		{ "<leader>g0", "<cmd>Grapple select index=10<cr>", desc = "[G]rapple [10]" },
 
 		{ "<leader>gn", "<cmd>Grapple cycle_tags next<cr>", desc = "[G]rapple [N]ext" },
 		{ "<leader>gp", "<cmd>Grapple cycle_tags prev<cr>", desc = "[G]rapple [P]revious" },

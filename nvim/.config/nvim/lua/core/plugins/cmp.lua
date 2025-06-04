5f7d91fbd9f406247f1e962de8cdb39ab40573d9
Niklas Vaethbrueckner <n.vaethbrueckner@snapaddy.com>
2025-06-04 21:36:26 +0200

    fix(nvim): uncommented copilot buffer

--- nvim/.config/nvim/lua/core/plugins/cmp.lua
+++ nvim/.config/nvim/lua/core/plugins/cmp.lua
@@ -132,8 +132,8 @@ return {
 					{ name = "nvim_lsp", group_index = 1 },
 					{ name = "nvim_lsp_signature_help", group_index = 1 },
 					{ name = "path", group_index = 1 },
-					{ name = "copilot", group_index = 1 },
-					{ name = "buffer", group_index = 2 },
+					-- { name = "copilot", group_index = 1 },
+					{ name = "buffer", group_index = 1 },
 					-- { name = "sql" },
 				},
 			})

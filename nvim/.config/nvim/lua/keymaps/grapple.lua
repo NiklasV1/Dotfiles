1c0bc49f0653d781f3b521f607292e95325d9e05
niklas <niklas@p200300ee372fc70447749fe529fff010.dip0.t-ipconnect.de>
2026-01-24 19:14:03 +0100

    refactor(nvim): refactored config

---
+++ nvim/.config/nvim/lua/keymaps/grapple.lua
@@ -0,0 +1,18 @@
+return {
+	{ "<leader>ga", "<cmd>Grapple toggle<cr>", desc = "[G]rapple [A]dd" },
+	{ "<leader>go", "<cmd>Grapple toggle_tags<cr>", desc = "[G]rapple [O]pen" },
+
+	{ "<leader>g1", "<cmd>Grapple select index=1<cr>", desc = "[G]rapple [1]" },
+	{ "<leader>g2", "<cmd>Grapple select index=2<cr>", desc = "[G]rapple [2]" },
+	{ "<leader>g3", "<cmd>Grapple select index=3<cr>", desc = "[G]rapple [3]" },
+	{ "<leader>g4", "<cmd>Grapple select index=4<cr>", desc = "[G]rapple [4]" },
+	{ "<leader>g5", "<cmd>Grapple select index=5<cr>", desc = "[G]rapple [5]" },
+	{ "<leader>g6", "<cmd>Grapple select index=6<cr>", desc = "[G]rapple [6]" },
+	{ "<leader>g7", "<cmd>Grapple select index=7<cr>", desc = "[G]rapple [7]" },
+	{ "<leader>g8", "<cmd>Grapple select index=8<cr>", desc = "[G]rapple [8]" },
+	{ "<leader>g9", "<cmd>Grapple select index=9<cr>", desc = "[G]rapple [9]" },
+	{ "<leader>g0", "<cmd>Grapple select index=10<cr>", desc = "[G]rapple [10]" },
+
+	{ "<leader>gn", "<cmd>Grapple cycle_tags next<cr>", desc = "[G]rapple [N]ext" },
+	{ "<leader>gp", "<cmd>Grapple cycle_tags prev<cr>", desc = "[G]rapple [P]revious" },
+}

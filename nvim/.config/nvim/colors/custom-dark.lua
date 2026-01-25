6dffdb644e28d8408a124e01a558d11c638e7483
niklas <niklas@p200300ee372fc7a6186ed49f7dc51ef6.dip0.t-ipconnect.de>
2026-01-25 18:08:28 +0100

    refactor(nvim): refactored config

---
+++ nvim/.config/nvim/colors/custom-dark.lua
@@ -0,0 +1,40 @@
+-- Reset colors
+vim.cmd("highlight clear")
+vim.opt.background = "dark"
+vim.g.colors_name = "custom-dark"
+
+local hl = vim.api.nvim_set_hl
+
+-- Cursor
+-- CursorLine
+-- ErrorMsg
+-- WinSeparator
+-- VertSplit
+-- SignColumn
+-- IncSearch
+-- LineNr
+-- CursorLineNr
+-- MatchParen
+-- ModeMsg
+-- MoreMsg
+-- NonText
+-- Normal
+-- NormalFloat
+-- FloatBorder
+-- Pmenu
+-- PmenuSel
+-- PmenuSbar
+-- PmenuThumb
+-- Question
+-- Search
+-- SpecialKey
+-- StatusLine
+-- StatusLineNC
+-- Title
+-- TabLine
+-- TabLineFill
+-- TabLineSel
+-- Visual
+-- VisualNOS
+-- WarningMsg
+-- WildMenu

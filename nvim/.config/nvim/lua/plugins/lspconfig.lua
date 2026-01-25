cc5c87cf82d4c7b39054052bc7d61444e469a6d4
niklas <niklas@p200300ee372fc7a6186ed49f7dc51ef6.dip0.t-ipconnect.de>
2026-01-25 12:25:03 +0100

    refactor(nvim): refactored config

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -88,21 +88,6 @@ return {
 					"bashls",
 				},
 			})
-
-			-- Auto-Install servers
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to format Lua code
-			})
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-			-- Setup server configs
-			for server_name, server_config in pairs(servers) do
-				vim.lsp.config(server_name, server_config)
-			end
-
-			-- Enable servers
-			require("mason-lspconfig").setup()
 		end,
 	},
 }

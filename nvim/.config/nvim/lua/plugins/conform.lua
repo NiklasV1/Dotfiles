a644c17cd6a6131a686f2572570bc5e41e16ffc6
niklas <niklas@p200300ee372fc7a6186ed49f7dc51ef6.dip0.t-ipconnect.de>
2026-01-25 15:04:43 +0100

    refactor(nvim): refactored config

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,4 +1,4 @@
-local prettier = "prettier"
+local prettier = "prettierd"
 
 return {
 	"stevearc/conform.nvim",
@@ -6,20 +6,10 @@ return {
 	cmd = { "ConformInfo" },
 	opts = {
 		notify_on_error = false,
-		format_on_save = function(bufnr)
-			-- Why is htmlangular disabled?
-			local disable_filetypes = { c = true, cpp = true, sql = true, htmlangular = true, htmldjango = true }
-			local lsp_format_opt
-			if disable_filetypes[vim.bo[bufnr].filetype] then
-				lsp_format_opt = "never"
-			else
-				lsp_format_opt = "fallback"
-			end
-			return {
-				timeout_ms = 2000,
-				lsp_format = lsp_format_opt,
-			}
-		end,
+		format_on_save = {
+			timeout_ms = 2000,
+			lsp_format = "fallback",
+		},
 		formatters_by_ft = {
 			lua = { "stylua" },
 			typescript = { prettier, stop_after_first = true },
--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -12,7 +12,7 @@ local tools = {
 	"angularls",
 	"tailwindcss",
 	"eslint",
-	-- "prettierd",
+	"prettierd",
 
 	-- Misc
 	"typos_lsp",

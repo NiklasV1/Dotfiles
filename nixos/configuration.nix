e1f9464b3850bdd3d67ef24d7566e048504ed5cb
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2026-04-05 00:20:07 +0200

    fix(nixos): fixed rename warnings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -48,8 +48,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -89,7 +89,7 @@
   # Enable zsh with Oh My Zsh
   programs.zsh = {
     enable = true;
-    oh-my-zsh = {
+    ohMyZsh = {
       enable = true;
       theme = "robbyrussell";
     };

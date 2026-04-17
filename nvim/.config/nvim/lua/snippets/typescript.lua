d0149a9242b96f05e6fe36036d03fe594ef93cce
Niklas Vaethbrueckner <n.vaethbrueckner@snapaddy.com>
2026-04-17 17:01:46 +0200

    feat(nvim): added additional typescript snippets

--- nvim/.config/nvim/lua/snippets/typescript.lua
+++ nvim/.config/nvim/lua/snippets/typescript.lua
@@ -11,9 +11,11 @@ local d = ls.dynamic_node
 local c = ls.choice_node
 
 return {
-	-- Console log
+	-- Console log: console.log('x', x)
 	s("cons", {
-		t("console.log("),
+		t("console.log('"),
+		f(function(args) return args[1][1] end, { 1 }),
+		t("', "),
 		i(1, "value"),
 		t(");"),
 	}),
@@ -43,6 +45,53 @@ return {
 		"af",
 		fmt([[(|args/) => { |body/ }]], {
 			args = i(1, "args"),
+			body = i(0),
+		}, { delimiters = "|/" })
+	),
+
+	-- Method definition
+	s(
+		"meth",
+		fmt([[|name/(|arg/: |argType/): |returnType/ {
+	|body/
+}]], {
+			name = i(1, "methodName"),
+			arg = i(2, "arg"),
+			argType = i(3, "ArgType"),
+			returnType = i(4, "ReturnType"),
+			body = i(0),
+		}, { delimiters = "|/" })
+	),
+
+	-- Type definition
+	s(
+		"type",
+		fmt([[type |name/ = {
+	|field/: |fieldType/;
+}]], {
+			name = i(1, "Name"),
+			field = i(2, "field"),
+			fieldType = i(3, "FieldType"),
+		}, { delimiters = "|/" })
+	),
+
+	-- Enum definition
+	s(
+		"enum",
+		fmt([[enum |name/ {
+	|value/,
+}]], {
+			name = i(1, "Name"),
+			value = i(2, "VALUE"),
+		}, { delimiters = "|/" })
+	),
+
+	-- ICONS const definition
+	s(
+		"icons",
+		fmt([[protected readonly ICONS = {
+	|body/
+} as const;]], {
 			body = i(0),
 		}, { delimiters = "|/" })
 	),

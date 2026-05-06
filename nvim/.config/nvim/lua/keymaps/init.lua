e44499f9f8824054ffe4d92780948d56fe97f8b0
Niklas Vaethbrueckner <n.vaethbrueckner@snapaddy.com>
2026-05-06 17:41:40 +0200

    feat(nvim): improved keybinds

--- nvim/.config/nvim/lua/keymaps/init.lua
+++ nvim/.config/nvim/lua/keymaps/init.lua
@@ -30,10 +30,16 @@ vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 
 -- Splits
+vim.keymap.set("n", "<Left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<Right>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<Down>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<Up>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
 vim.keymap.set("n", "<leader>v", function()
 	vim.cmd("vsplit")
 end, { desc = "Split [V]ertically" })
--- nvim/.config/nvim/lua/keymaps/leap.lua
+++ nvim/.config/nvim/lua/keymaps/leap.lua
@@ -1,2 +1,3 @@
+vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)", { desc = "[f]ind" })
 vim.keymap.set({ "n", "x", "o" }, "<Leader>f", "<Plug>(leap)", { desc = "[f]ind" })
 vim.keymap.set("n", "<Leader>F", "<Plug>(leap-from-window)", { desc = "[F]ind from window" })

c5f46e3d699b04dd823022caf2cef6b3bed6defc
Niklas Vaethbrueckner <n.vaethbrueckner@snapaddy.com>
2026-05-20 10:42:24 +0200

    feat(nvim): added rush quickfix parsing

--- nvim/.config/nvim/lua/keymaps/rush.lua
+++ nvim/.config/nvim/lua/keymaps/rush.lua
@@ -1,5 +1,6 @@
 local executeCommands = require("utils.utils").executeCommands
 local goToFeaturePath = require("utils.work-utils").goToFeaturePath
+local getBackendFeaturePath = require("utils.work-utils").getBackendFeaturePath
 
 -- Commands
 local UPDATE = "rush update" -- Changes lockfiles
@@ -26,12 +27,59 @@ local function rushStartFeat()
 	executeCommands({ goToFeaturePath(), START_FEATURE })
 end
 
+local function rushXBuildQF()
+	local feature_path = getBackendFeaturePath()
+	local cmd = "cd " .. feature_path .. " && " .. BUILD_FEATURE
+	local output_lines = {}
+
+	vim.notify("Rush build started...", vim.log.levels.INFO)
+
+	vim.fn.jobstart({ "bash", "-c", cmd }, {
+		stdout_buffered = false,
+		on_stdout = function(_, data)
+			for _, line in ipairs(data) do
+				table.insert(output_lines, line)
+			end
+		end,
+		on_stderr = function(_, data)
+			for _, line in ipairs(data) do
+				table.insert(output_lines, line)
+			end
+		end,
+		on_exit = function()
+			local qf_items = {}
+			local pattern = "^(.+)%((%d+),%d+%): error TS%d+: (.+)$"
+
+			for _, line in ipairs(output_lines) do
+				local file, lnum, msg = line:match(pattern)
+				if file then
+					table.insert(qf_items, {
+						filename = feature_path .. file,
+						lnum = tonumber(lnum),
+						text = msg,
+						type = "E",
+					})
+				end
+			end
+
+			if #qf_items == 0 then
+				vim.notify("Rush build: No errors!", vim.log.levels.INFO)
+			else
+				vim.fn.setqflist(qf_items, "r")
+				vim.notify("Rush build: " .. #qf_items .. " error(s) found", vim.log.levels.WARN)
+				vim.cmd("bot copen")
+			end
+		end,
+	})
+end
+
 -- Keybinds
 local function loadKeybinds()
 	vim.keymap.set("n", "<leader>rb", rushBuild, { desc = "[R]ush [B]uild" })
 	vim.keymap.set("n", "<leader>rxb", rushXBuild, { desc = "[R]ush[X] [B]uild" })
 	vim.keymap.set("n", "<leader>rf", rushFull, { desc = "[R]ush [F]ull" })
 	vim.keymap.set("n", "<leader>rsf", rushStartFeat, { desc = "[R]ush [S]tart [F]eat" })
+	vim.keymap.set("n", "<leader>rxq", rushXBuildQF, { desc = "[R]ush[X] [B]uild [Q]uickfix" })
 end
 
 -- Load keybinds
